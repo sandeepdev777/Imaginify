51ddfaf196cfcf522b5b9476a625a1af1ea145cb
Sandeep Kumar <sandeepkumar07007@gmail.com>
2024-03-15 23:55:46 +0530

    Your descriptive commit message

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 //modified
 import { navLinks } from "@/constants"
-
 import Image from "next/image"
 import Link from "next/link"
 
@@ -10,27 +9,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
 
   return (
     <>
-      <section className="home">
-        <h1 className="home-heading">
-          Unleash Your Creative Vision with Imaginify
-        </h1>
-        <ul className="flex-center w-full gap-20">
-          {navLinks.slice(1, 5).map((link) => (
-            <Link
-              key={link.route}
-              href={link.route}
-              className="flex-center flex-col gap-2"
-            >
-              <li className="flex-center w-fit rounded-full bg-white p-4">
-                <Image src={link.icon} alt="image" width={24} height={24} />
-              </li>
-              <p className="p-14-medium text-center text-white">{link.label}</p>
-            </Link>
-          ))}
-        </ul>
-      </section>
-
-    
+      
     </>
   )
 }
