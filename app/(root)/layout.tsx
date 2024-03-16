80fb88b184bbf73f17fc1f958f171a77d764cc18
Sandeep Kumar <sandeepkumar07007@gmail.com>
2024-03-16 16:38:23 +0530

    Your descriptive commit message

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,20 +1,24 @@
 
-import MobileNav from '@/components/shared/MobileNav'
-import Sidebar from '@/components/shared/Sidebar'
+import React from 'react';
+import MobileNav from '@/components/shared/MobileNav';
+import Sidebar from '@/components/shared/Sidebar';
 
-// React.ReactNode is a type for anything that can be rendered in React. It makes the children prop flexible enough to accept any kind of content that can be rendered in React.
-const Layout = ({children}:{children:React.ReactNode}) => {
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='root'>
-     <Sidebar />
-     <MobileNav/>
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+
       <div className="root-container">
         <div className="wrapper">
-          {children}
+          <React.Fragment> // Wrap children prop inside a React fragment
+            {children}
+          </React.Fragment>
         </div>
       </div>
     </main>
-  )
+  );
 }
 
 export default Layout
